f6279be2956d577075df891f69e65ba31c5e3456
bezrukiy <66155368+bezrukiy@users.noreply.github.com>
2021-05-20 13:14:39 +0600

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Лаб1.MenuItem;
+using Лаб1.MenuItems;
+
+namespace Лаб1
+{
+    public class Menu
+    {
+        private static List<MenuItemCore> MenuItems = new List<MenuItemCore>();
+
+        public static int ItemsCount
+        {
+            get
+            {
+                return MenuItems.Count();
+            }
+        }
+
+        public static void ClearItems()
+        {
+            Menu.MenuItems.Clear();
+        }
+
+        public static void AddItem(MenuItemCore menuItem)
+        {
+            Menu.MenuItems.Add(menuItem);
+        }
+
+        public static void AddItem(string title, MenuItemWDelegate.ExecuteHandler handler)
+        {
+            Menu.MenuItems.Add(new MenuItemWDelegate(title, handler));
+        }
+
+        public static void Execute()
+        {
+            int iMenu = IOUtils.SafeReadInteger("mi", null) - 1;
+            if (iMenu >= 0 && iMenu < Menu.MenuItems.Count)
+            {
+                Menu.MenuItems.ToArray()[iMenu].Execute();
+            }
+            else
+            {
+                Console.WriteLine("Menu item not found.");
+            }
+        }
+        public static void ShowMenu()
+        {
+            Console.WriteLine("=== MENU ===");
+
+            int iMenuItem = 1;
+            foreach (MenuItemCore menuItem in Menu.MenuItems)
+            {
+                Console.WriteLine("{0}: {1}", iMenuItem++, menuItem.Title);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Лаб1.MenuItem;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Лаб1.MenuItems;
+
+namespace Лаб1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDictionary<string, string> argsDic = CmdArgsParser.Parse(args);
+            IOUtils.SetExtValues(argsDic);
+
+            Menu.ClearItems();
+            Menu.AddItem(new MenuItemExit());
+            Menu.AddItem("Hello world!", PrintHelloWorld);
+            Menu.AddItem(new MenuItemCalc());
+            Menu.AddItem(new MenuItemDate());
+            Menu.AddItem(new MenuItemSafeIntReading());
+
+            if (argsDic != null)
+            {
+                try
+                {
+                    Menu.Execute();
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine("ERROR: " + ex.Message);
+                }
+            }
+            else
+            {
+                while (true)
+                {
+                    Menu.ShowMenu();
+                    Menu.Execute();
+                }
+            }
+        }
+
+        public static void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+
+
+    struct Point
+    {
+        int X;
+        int Y;
+    }
+
+    interface IList
+    {
+        int Count { get; }
+        void AddItem();
+        void Clear();
+    }
+}
+
+
+ 
